a475106a98bb9000364ae553249dd2ffd93f26a3
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2009-02-12 01:54:50 +0000

    Removing seleium-server startup and shutdown. That doesn't make sense because Pyccuracy already does it.

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -4,19 +4,10 @@ import unittest
 from pyccuracy_core import Pyccuracy
 
 class TestStoryRunner(unittest.TestCase):
-    svr = None
-    
-    def setUp(self):
-        self.svr = SeleniumServer()        
-        self.svr.run()
     
     def test_runner(self):
         pyc = Pyccuracy()
         pyc.run_tests()
-        self.assertNotEquals(pyc,None)
-        
-    def tearDown(self):
-        self.svr.stop()
-
+    
 if __name__ == "__main__":
     unittest.main()
