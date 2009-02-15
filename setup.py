3d5a03b6607a59d197b0e1ff1771cd9fc0c5531f
Bernardo Heynemann <heynemann@gmail.com>
2009-02-15 17:56:19 +0000

    Updated PyPI text.

--- setup.py
+++ setup.py
@@ -13,6 +13,30 @@ Introduction
 
 Pyccuracy is a BDD style Acceptance Testing framework.
 
+A typical Pyccuracy test would be something like::  
+
+  As a Google User
+  I want to search Google
+  So that I can test Pyccuracy
+
+  Scenario 1 - Searching for Hello World
+  Given
+    I go to "http://www.google.com"
+  When
+    I fill "q" textbox with "Hello World"
+    And I click "btnG" button
+  Then
+    I see "Hello World - Pesquisa Google" title
+ 
+  Scenario 2 - Searching for Monty Python
+  Given
+    I go to "http://www.google.com"
+  When
+    I fill "q" textbox with "Monty Python"
+    And I click "btnG" button
+  Then
+    I see "Monty Python - Pesquisa Google" title
+
 ===================
 Project Cheat Sheet
 ===================
