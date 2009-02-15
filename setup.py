94f23f803ec1fd63962b1d9521976946303cc46d
Bernardo Heynemann <heynemann@gmail.com>
2009-02-15 17:34:50 +0000

    Release 880.

--- setup.py
+++ setup.py
@@ -28,7 +28,46 @@ Project Subversion Server: http://svn.stormwindproject.org/svn/Pyccuracy (``svn 
 
 **PyPI Page**: http://pypi.python.org/pypi/Pyccuracy/0.1dev-r879
 
-**Docs for current version**: http://packages.python.org/Pyccuracy/""",
+**Docs for current version**: http://packages.python.org/Pyccuracy/
+
+=============
+Release Notes
+=============
+
+Release Notes - Pyccuracy - Version 0.1
+---------------------------------------
+**Bug**
+
+    [PYCCURACY-66 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-66] - Fix running the pyccuracy_core on *nix machines.
+
+**New Feature**
+
+    [PYCCURACY-14 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-14] - Check/Uncheck Checkbox Action
+    [PYCCURACY-15 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-15] - Checkbox Is Checked Action
+    [PYCCURACY-16 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-16] - Checkbox Is Not Checked Action
+    [PYCCURACY-17 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-17] - See Checkbox Action
+    [PYCCURACY-21 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-21] - See Button Action
+    [PYCCURACY-40 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-40] - See Select Action
+    [PYCCURACY-41 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-41] - Do Not See Select Action
+    [PYCCURACY-42 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-42] - See Select Has Selected Index of Action
+    [PYCCURACY-50 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-50] - Select Option By Index
+    [PYCCURACY-57 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-57] - Click Image Action
+    [PYCCURACY-58 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-58] - See Link Action
+    [PYCCURACY-63 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-63] - Do Not See Link Action
+    [PYCCURACY-64 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-64] - Click Link Action
+    [PYCCURACY-80 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-80] - Render a summary of the run after the run
+    [PYCCURACY-87 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-87] - Wait for Page To Load Action
+
+**Task**
+
+    [PYCCURACY-4 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-4] - Create documentation for current release
+    [PYCCURACY-9 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-9] - Encapsulate Pyccuracy on a .egg
+    [PYCCURACY-10 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-10] - Create index page for Pyccuracy.egg distribution
+    [PYCCURACY-84 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-84] - Add tests for Click Button
+    [PYCCURACY-85 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-85] - Add tests for Type Text
+    [PYCCURACY-86 - http://jira.stormwindproject.org:8080/browse/PYCCURACY-86] - Add tests for See Title
+
+""",
       classifiers=["Development Status :: 2 - Pre-Alpha",
 				   "Intended Audience :: Developers",
 				   "License :: OSI Approved",
