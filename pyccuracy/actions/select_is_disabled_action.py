fce1ff5e247daaca1bb10d2f3d58463f65ad12ab
Bernardo Heynemann <heynemann@gmail.com>
2009-02-25 00:21:25 +0000

    Fixes PYCCURACY-49 and PYCCURACY-48.

---
+++ pyccuracy/actions/select_is_disabled_action.py
@@ -0,0 +1,27 @@
+import re
+from element_selector import *
+from action_base import *
+
+class SelectIsDisabledAction(ActionBase):
+    def __init__(self, browser_driver, language):
+        super(SelectIsDisabledAction, self).__init__(browser_driver, language)
+
+    def get_selector(self, element_name):
+        return ElementSelector.select(element_name)
+
+    def matches(self, line):
+        reg = self.language["select_is_disabled_regex"]
+        self.last_match = reg.search(line)
+        return self.last_match
+
+    def values_for(self, line):
+        return self.last_match and (self.last_match.groups()[1],) or tuple([])
+
+    def execute(self, values):
+        link_name = values[0]		
+        link = self.get_selector(link_name)
+        self.assert_element_is_visible(link, self.language["select_is_visible_failure"] % link_name)        
+        
+        error_message = self.language["select_is_disabled_failure"]
+        if not self.browser_driver.is_element_enabled(link):
+            self.raise_action_failed_error(error_message % link_name)
---
+++ tests/action_tests/test_one.py
@@ -0,0 +1,21 @@
+import unittest
+from action_test_base import *
+
+class TestOne(ActionTestBase):
+
+    def set_pattern(self, pattern):
+        self.pattern = pattern
+
+    def get_pattern(self, culture):
+        return "*%s_%s.acc" % (self.pattern, culture)
+
+    def test_each_language(self, other):
+        self.run_tests()
+
+if __name__ == "__main__":
+    test = TestOne("test_each_language")
+    test.setUp()
+    test.set_pattern(sys.argv[-1])
+    runner = unittest.TextTestRunner()
+    runner.run(test.test_each_language)
+
