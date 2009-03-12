d70edd7376f4070120641d42401429505a792794
Bernardo Heynemann <heynemann@gmail.com>
2009-03-11 22:37:58 -0300

    Fixes PYCCURACY-53.

---
+++ pyccuracy/actions/image_is_visible_action.py
@@ -0,0 +1,24 @@
+import os
+import sys
+sys.path.insert(0,os.path.abspath(__file__+"/../../../"))
+from pyccuracy.page import Page
+from pyccuracy.actions.action_base import ActionBase
+from pyccuracy.actions.element_is_visible_base import *
+
+class ImageIsVisibleAction(ElementIsVisibleBase):
+    def __init__(self, browser_driver, language):
+        super(ImageIsVisibleAction, self).__init__(browser_driver, language)
+
+    def matches(self, line):
+        reg = self.language["image_is_visible_regex"]
+        self.last_match = reg.search(line)
+        return self.last_match
+
+    def values_for(self, line):
+        return self.last_match and (self.last_match.groups()[1],) or tuple([])
+
+    def execute(self, values, context):
+        image_name = values[0]
+        error_message = self.language["image_is_visible_failure"]
+        self.execute_is_visible(context, Page.Image, image_name, error_message)
+        
