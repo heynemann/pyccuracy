166492f361602fe65864cb719348d0e1adcb49d0
Bernardo Heynemann <heynemann@gmail.com>
2009-03-29 01:48:35 -0300

    First server that really does something...

---
+++ pyccuracy/pyccuracy_distributed_language.py
@@ -0,0 +1,6 @@
+DEFAULT_PORT = 4587
+BUFFER_SIZE = 8192
+identify_action = "@identify"
+identify_message = identify_action + ":%s"
+get_next_test_action = "@get-next-test"
+get_next_test_message = get_next_test_action
