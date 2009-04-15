d41173517d3153b8086208a6e32f80619b61f9cd
Bernardo Heynemann <bernardo@corp.globo.com>
2009-04-15 14:15:33 -0300

    Added force_unicode to test results.

--- pyccuracy/pyccuracy_core.py
+++ pyccuracy/pyccuracy_core.py
@@ -28,6 +28,24 @@ from actions.action_base import ActionBase
 import report_parser as report
 
 class PyccuracyCore(object):
+    def force_unicode(self, s, encoding='utf-8', errors='strict'):
+        #print "Decoding %s with encoding %s" % (s, encoding)
+        if not isinstance(s, basestring,):
+            if hasattr(s, '__unicode__'):
+                s = unicode(s)
+            else:
+                try:
+                    s = unicode(str(s), encoding, errors)
+                except UnicodeEncodeError:
+                    if not isinstance(s, Exception):
+                        raise
+                    s = ' '.join([self.force_unicode(arg, encoding, errors) for arg in s])
+        elif not isinstance(s, unicode):
+            s = s.decode(encoding, errors)
+
+        return s
+
+
     def configure_context(self,
                           tests_dir,
                           actions_dir,
@@ -213,7 +231,7 @@ class PyccuracyCore(object):
         return lang
 
     def __print_results(self, results):
-        print unicode(results)
+        print self.force_unicode(results)
         print "\n"
 
 class PyccuracyContext:
