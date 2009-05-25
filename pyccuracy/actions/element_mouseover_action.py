5fb031b179bea722e86bd405898c2cc12c3a9818
Bernardo Heynemann <bernardo@corp.globo.com>
2009-05-25 14:52:23 -0300

    Element mouseover action.

--- pyccuracy/actions/element_mouseover_action.py
+++ pyccuracy/actions/element_mouseover_action.py
@@ -19,21 +19,22 @@ from pyccuracy.page import Page
 from pyccuracy.actions.action_base import ActionBase
 from pyccuracy.actions.element_is_visible_base import *
 
-class LinkMouseoverAction(ActionBase):
+class ElementMouseoverAction(ActionBase):
     def __init__(self, browser_driver, language):
-        super(LinkMouseoverAction, self).__init__(browser_driver, language)
+        super(ElementMouseoverAction, self).__init__(browser_driver, language)
 
     def matches(self, line):
-        reg = self.language["link_mouseover_regex"]
+        reg = self.language["element_mouseover_regex"]
         self.last_match = reg.search(line)
         return self.last_match
 
     def values_for(self, line):
-        return self.last_match and (self.last_match.groups()[1],) or tuple([])
+        return self.last_match.groupdict()
 
     def execute(self, values, context):
-        link_name = values[0]
-        link = self.resolve_element_key(context, Page.Link, link_name)
-        self.assert_element_is_visible(link, self.language["element_is_visible_failure"] % ("link", link_name))
-        self.browser_driver.mouseover_element(link)
-        
+        element_name = values["element_key"]
+        element_type = values["element_type"]
+        element = self.resolve_element_key(context, element_type, element_name)
+        self.assert_element_is_visible(element, self.language["element_is_visible_failure"] % (element_type, element_name))
+        self.browser_driver.mouseover_element(element)
+
