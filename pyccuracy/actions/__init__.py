e42ff78a3785d8fbc71bba584f2f5dcf6606daf3
Gabriel Falcão <gabriel.falcao@corp.globo.com>
2009-05-28 15:45:10 -0300

    Removing useless code

--- pyccuracy/actions/__init__.py
+++ pyccuracy/actions/__init__.py
@@ -55,61 +55,8 @@ class MetaActionBase(type):
 
 class ActionBase(object):
     __metaclass__ = MetaActionBase
-
-    def __init__(self, browser_driver, language):
-        self.browser_driver = browser_driver
-        self.language = language
+    ActionFailedError = ActionFailedError
 
     @classmethod
     def can_resolve(cls, string):
         return re.match(cls.regex, string)
-
-    def raise_action_failed_error(self, message):
-        raise ActionFailedError(message)
-
-    def is_element_visible(self, selector):
-        is_visible = self.browser_driver.is_element_visible(selector)
-        return is_visible
-
-    def assert_element_is_visible(self, selector, message):
-        if not self.is_element_visible(selector):
-            self.raise_action_failed_error(message + "(Resolved to Element %s)" % selector)
-
-    def assert_element_is_not_visible(self, selector, message):
-        if self.is_element_visible(selector):
-            self.raise_action_failed_error(message + "(Resolved to Element %s)" % selector)
-
-    def resolve_element_key(self, context, element_type, element_key):
-        page = context.current_page
-        if page == None:
-            element_key = self.browser_driver.resolve_element_key(context, element_type, element_key)
-            return element_key
-
-        return page.get_registered_element(element_key) or self.browser_driver.resolve_element_key(context, element_type, element_key)
-
-    def is_element_empty(self, selector):
-        is_empty = self.browser_driver.is_element_empty(selector)
-        return is_empty
-
-    def assert_element_is_empty(self, selector, message):
-        if not self.is_element_empty(selector):
-            self.raise_action_failed_error(message)
-
-    def assert_element_is_not_empty(self, selector, message):
-        if self.is_element_empty(selector):
-            self.raise_action_failed_error(message)
-
-    def execute_action(self, action_text, context):
-        found = False
-        for action in context.all_custom_actions:
-            if action.__class__.__name__!="ActionBase" and action.matches(action_text):
-                action.execute(action.values_for(action_text), context)
-                found = True
-        for action in context.all_actions:
-            if action.__class__.__name__!="ActionBase" and action.matches(action_text):
-                action.execute(action.values_for(action_text), context)
-                found = True
-
-        if not found:
-            raise RuntimeError('The specified line("%s") didn\'t match any custom or built-in actions.' % (action_text,))
-
