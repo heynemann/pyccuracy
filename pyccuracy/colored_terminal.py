c2e7ac64d5f71333f31a45f3952a87ae78d2456c
Gabriel Falcão <gabriel.falcao@corp.globo.com>
2009-06-01 12:58:16 -0300

    Fixed the functional tests

--- pyccuracy/colored_terminal.py
+++ pyccuracy/colored_terminal.py
@@ -3,8 +3,8 @@ import sys, re
 class TerminalController:
     """
     A class that can be used to portably generate formatted output to
-    a terminal.  
-    
+    a terminal.
+
     `TerminalController` defines a set of instance variables whose
     values are initialized to the control sequence necessary to
     perform a given action.  These can be simply included in normal
@@ -64,11 +64,11 @@ class TerminalController:
 
     # Foreground colors:
     BLACK = BLUE = GREEN = CYAN = RED = MAGENTA = YELLOW = WHITE = ''
-    
+
     # Background colors:
     BG_BLACK = BG_BLUE = BG_GREEN = BG_CYAN = ''
     BG_RED = BG_MAGENTA = BG_YELLOW = BG_WHITE = ''
-    
+
     _STRING_CAPABILITIES = """
     BOL=cr UP=cuu1 DOWN=cud1 LEFT=cub1 RIGHT=cuf1
     CLEAR_SCREEN=clear CLEAR_EOL=el CLEAR_BOL=el1 CLEAR_EOS=ed BOLD=bold
@@ -100,7 +100,7 @@ class TerminalController:
         # Look up numeric capabilities.
         self.COLS = curses.tigetnum('cols')
         self.LINES = curses.tigetnum('lines')
-        
+
         # Look up string capabilities.
         for capability in self._STRING_CAPABILITIES:
             (attrib, cap_name) = capability.split('=')
@@ -144,50 +144,3 @@ class TerminalController:
         s = match.group()
         if s == '$$': return s
         else: return getattr(self, s[2:-1])
-
-#######################################################################
-# Example use case: progress bar
-#######################################################################
-
-class ProgressBar:
-    """
-    A 3-line progress bar, which looks like::
-    
-                                Header
-        20% [===========----------------------------------]
-                           progress message
-
-    The progress bar is colored, if the terminal supports color
-    output; and adjusts to the width of the terminal.
-    """
-    BAR = '%3d%% ${GREEN}[${BOLD}%s%s${NORMAL}${GREEN}]${NORMAL}\n'
-    HEADER = '${BOLD}${CYAN}%s${NORMAL}\n\n'
-        
-    def __init__(self, term, header):
-        self.term = term
-        if not (self.term.CLEAR_EOL and self.term.UP and self.term.BOL):
-            raise ValueError("Terminal isn't capable enough -- you "
-                             "should use a simpler progress dispaly.")
-        self.width = self.term.COLS or 75
-        self.bar = term.render(self.BAR)
-        self.header = self.term.render(self.HEADER % header.center(self.width))
-        self.cleared = 1 #: true if we haven't drawn the bar yet.
-        self.update(0, '')
-
-    def update(self, percent, message):
-        if self.cleared:
-            sys.stdout.write(self.header)
-            self.cleared = 0
-        n = int((self.width-10)*percent)
-        sys.stdout.write(
-            self.term.BOL + self.term.UP + self.term.CLEAR_EOL +
-            (self.bar % (100*percent, '='*n, '-'*(self.width-10-n))) +
-            self.term.CLEAR_EOL + message.center(self.width))
-
-    def clear(self):
-        if not self.cleared:
-            sys.stdout.write(self.term.BOL + self.term.CLEAR_EOL +
-                             self.term.UP + self.term.CLEAR_EOL +
-                             self.term.UP + self.term.CLEAR_EOL)
-            self.cleared = 1
-
--- tests/functional/test_result.py
+++ tests/functional/test_result.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
-
+import re
 from pyccuracy.result import Result
 from pyccuracy.common import Settings
 from pyccuracy.fixture import Fixture
@@ -9,7 +9,7 @@ from pyccuracy.fixture_items import Story, Scenario, Action
 def complete_scenario_with_then_action_returned():
     story = Story(as_a="Someone", i_want_to="Do Something", so_that="I'm Happy", identity="some file")
     scenario = story.append_scenario("1", "Something")
-    given = scenario.add_given(action_description="I did something", execute_function=lambda: None, args=["s"], kwargs={"a":"b"})
+    given = scenario.add_given(action_description="I did something", execute_function=lambda: None, args=["s"], kwargs={"a":"bs"})
     when = scenario.add_when(action_description="I do something", execute_function=lambda: None, args=["s"], kwargs={"a":"b"})
     then = scenario.add_then(action_description="Something happens", execute_function=lambda: None, args=["s"], kwargs={"a":"b"})
     return then
@@ -18,15 +18,14 @@ def test_see_summary_for_fixture_returns_proper_failed_scenarios_string():
     expected = u"""================
 Test Run Summary
 ================
-
 Status: FAILED
 
 Test Data Stats
 ---------------
-Successful Stories......0/1 (0.00%)
-Failed Stories..........1/1 (100.00%)
-Successful Scenarios....0/1 (0.00%)
-Failed Scenarios........1/1 (100.00%)
+Successful Stories......0 of 1 (0.00%)
+Successful Scenarios....0 of 1 (0.00%)
+Failed Stories..........1 of 1 (100.00%)
+Failed Scenarios........1 of 1 (100.00%)
 
 Failed Stories / Scenarios
 --------------------------
@@ -38,7 +37,8 @@ Scenario.......1 - Something
     When
         I do something - UNKNOWN
     Then
-        Something happens - FAILED - Something very bad happened"""
+        Something happens - FAILED - Something very bad happened
+"""
 
     settings = Settings()
     fixture = Fixture()
@@ -47,7 +47,7 @@ Scenario.......1 - Something
     fixture.append_story(action.scenario.story)
     action.mark_as_failed("Something very bad happened")
 
-    summary = result.summary_for("en-us")
+    summary = re.sub(r'[$][{][^}]+[}]', '', result.summary_for("en-us"))
 
     assert summary.strip() == expected.strip(), compare(summary.strip(), expected.strip())
 
@@ -55,12 +55,16 @@ def compare(str_a, str_b):
 
     for index in range(len(str_a)):
         if index > len(str_b):
-            print "Strings differ at position %d" % index
-            return
+            return  "Strings differ at position %d" % index
 
         char_a = str_a[index:index+1]
         char_b = str_b[index:index+1]
         if char_a != char_b:
-            print "Strings differ at position %d\nString A: %s\nString B: %s" % (index, str_a[:index+1], str_b[:index+1])
-            return
+            x =  "Strings differ at position %d\nString A: %s\nString B: %s\n" % (index, str_a[:index+1], str_b[:index+1])
+            x += "The error is that  %r is different of %r\n" % (str_a[index], str_b[index])
+            def diff(string, index):
+                return "[%s]%s" % (string[index], string[index+1:index+10])
+
+            x += "The difference is between %r and %r\n" % (diff(str_a, index), diff(str_b, index))
+            return x
 
