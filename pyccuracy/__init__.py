2bfe20a7eafa762c8e0baf4df967ea50c7cecd4f
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2009-07-29 10:43:00 -0300

    Release of version 1.0.4.

--- pyccuracy/__init__.py
+++ pyccuracy/__init__.py
@@ -22,5 +22,5 @@ from pyccuracy.page import PageRegistry, Page
 from pyccuracy.drivers import DriverRegistry
 from pyccuracy.drivers.core import *
 
-Version = "1.0.3"
+Version = "1.0.4"
 Release = "Viagra"
