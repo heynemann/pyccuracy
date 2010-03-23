ad059b6df68b55c5eef2239925b5d1d03ff1d179
Bernardo Heynemann <bernardo@corp.globo.com>
2010-03-22 21:52:27 -0300

    Release 1.2.9

--- pyccuracy/__init__.py
+++ pyccuracy/__init__.py
@@ -22,5 +22,5 @@ from pyccuracy.page import PageRegistry, Page
 from pyccuracy.drivers import DriverRegistry
 from pyccuracy.drivers.core import *
 
-Version = "1.2.8"
+Version = "1.2.9"
 Release = "Clarytin"
