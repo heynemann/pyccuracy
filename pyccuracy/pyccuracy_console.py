12d276d0f1ad59cc3ad414fa10e2d1d0c3e6b5aa
Bernardo Heynemann <heynemann@gmail.com>
2010-05-18 16:02:22 -0300

    Fixed the dependency that Pyccuracy console had on a fixed release.

--- pyccuracy/pyccuracy_console.py
+++ pyccuracy/pyccuracy_console.py
@@ -1,9 +1,11 @@
 #!/usr/local/virtual/bin/python
 # EASY-INSTALL-ENTRY-SCRIPT: 'Pyccuracy==1.2.25','console_scripts','pyccuracy_console'
-__requires__ = 'Pyccuracy==1.2.25'
+
+from pyccuracy import Version
+__requires__ = 'Pyccuracy==%s' % Version
 import sys
 from pkg_resources import load_entry_point
 
 sys.exit(
-   load_entry_point('Pyccuracy==1.2.25', 'console_scripts', 'pyccuracy_console')()
+   load_entry_point('Pyccuracy==%s' % Version, 'console_scripts', 'pyccuracy_console')()
 )
