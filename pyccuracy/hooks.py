c5b5d4cbd3f38c9e7d71b4e9a1ff52750608f4de
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2010-06-27 02:56:13 -0300

    Raise HookError when hook does not work.

--- pyccuracy/hooks.py
+++ pyccuracy/hooks.py
@@ -4,7 +4,14 @@ class Hooks(object):
     @classmethod
     def execute_after_tests(cls, results):
         for hook in HOOKS['after_tests']:
-            hook().execute(results)
+            try:
+                hook().execute(results)
+            except Exception, e:
+                raise HookError(e)
+    
+    @classmethod
+    def reset(cls):
+        HOOKS['after_tests'] = []
 
 class MetaHookBase(type):
     def __init__(cls, name, bases, attrs):
@@ -20,3 +27,13 @@ class MetaHookBase(type):
 
 class AfterTestsHook(object):
     __metaclass__ = MetaHookBase
+
+class HookError(Exception):
+    def __init__(self, origin):
+        self.origin = origin
+
+    def __str__(self):
+        return unicode(self)
+    
+    def __unicode__(self):
+        return "HookError: %s" % (self.origin)
--- tests/unit/test_hooks.py
+++ tests/unit/test_hooks.py
@@ -1,12 +1,14 @@
 from pmock import *
 from nose.tools import raises
+from time import sleep
 
-from pyccuracy.hooks import HOOKS, Hooks, AfterTestsHook
+from pyccuracy.hooks import HOOKS, Hooks, AfterTestsHook, HookError
 
 @raises(NotImplementedError)
 def test_construction_fails_without_implementing_execute():
     class DoNothing(AfterTestsHook):
         pass
+    Hooks.reset()
 
 def test_will_register_after_tests_hook():
     class SomeHook(AfterTestsHook):
@@ -14,6 +16,7 @@ def test_will_register_after_tests_hook():
             pass
 
     assert SomeHook in HOOKS['after_tests']
+    Hooks.reset()
 
 def test_will_execute_my_hook():
     result_mock = Mock()
@@ -25,3 +28,23 @@ def test_will_execute_my_hook():
     
     Hooks.execute_after_tests(result_mock)
     result_mock.verify()
+    Hooks.reset()
+
+@raises(HookError)
+def test_user_exceptions_make_pyccuracy_raises_hook_error():
+    class BrokenHook(AfterTestsHook):
+        def execute(self, results):
+            raise Exception("user did stupid things")
+    
+    Hooks.execute_after_tests(None)
+    Hooks.reset()
+
+def test_reset_hooks():
+    Hooks.reset()
+    class AHook(AfterTestsHook):
+        def execute(self, results):
+            pass
+    assert len(HOOKS['after_tests']) == 1
+    assert AHook in HOOKS['after_tests']
+    Hooks.reset()
+    assert len(HOOKS['after_tests']) == 0
