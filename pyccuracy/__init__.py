4f25b114f1dd941a216f1943b29acdce5f8d9347
Bernardo Heynemann <heynemann@gmail.com>
2010-08-25 11:35:11 -0300

    Pyccuracy init file... ops...

--- pyccuracy/__init__.py
+++ pyccuracy/__init__.py
@@ -22,5 +22,5 @@ from pyccuracy.page import PageRegistry, Page
 from pyccuracy.drivers import DriverRegistry
 from pyccuracy.drivers.core import *
 
-Version = "1.2.37"
+Version = "1.2.38"
 Release = "Clarytin"
