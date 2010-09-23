c2a4af7634d8a3e5fe93d6f3c23c9fedb05e3efd
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2010-09-23 16:20:12 -0300

    Release 1.2.43

--- pyccuracy/__init__.py
+++ pyccuracy/__init__.py
@@ -22,5 +22,5 @@ from pyccuracy.page import PageRegistry, Page
 from pyccuracy.drivers import DriverRegistry
 from pyccuracy.drivers.core import *
 
-Version = "1.2.42"
+Version = "1.2.43"
 Release = "Clarytin"
