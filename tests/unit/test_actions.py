1cb3f63337947c11e868655d5198d2be2b040741
Diogo Baeder de Paula Pinto <diogo.pinto@gonow.com.br>
2010-10-25 18:26:05 -0200

    test_actions.py ported to Mocker

--- tests/unit/test_actions.py
+++ tests/unit/test_actions.py
@@ -16,12 +16,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pmock import *
+from mocker import Mocker, ANY
 from nose.tools import raises, set_trace
 
 from pyccuracy import ActionBase, ActionRegistry
 from pyccuracy.languages import LanguageItem
 from pyccuracy.errors import LanguageDoesNotResolveError
+
+from utils import Object
 
 def test_construction():
     class DoNothingAction(ActionBase):
@@ -73,30 +75,49 @@ def test_cannot_resolve_string():
     assert not DoSomethingAction.can_resolve('Foo Bar')
 
 def test_action_registry_suitable_for_returns_my_action():
+    
+    mocker = Mocker()
+    
     class MyAction(ActionBase):
         regex = LanguageItem('foo_bar_regex')
         def execute(self, context, *args, **kw):
             pass
 
-    language_getter_mock = Mock()
-    language_getter_mock.expects(at_least_once()).method('get').will(return_value('^$'))
-    language_getter_mock.expects(once()).get(eq(LanguageItem('foo_bar_regex'))).will(return_value('My regex .+'))
-    Action, args, kwargs = ActionRegistry.suitable_for('My regex baz', 'en-us', getter=language_getter_mock)
-    language_getter_mock.verify()
-    assert Action is MyAction
+    language_getter_mock = mocker.mock()
+    language_getter_mock.get(LanguageItem('foo_bar_regex'))
+    mocker.count(min=1, max=1)
+    mocker.result('My regex .+')
+    language_getter_mock.get(ANY)
+    mocker.count(min=1, max=None)
+    mocker.result('^$')
+    
+
+    with mocker:
+        Action, args, kwargs = ActionRegistry.suitable_for('My regex baz', 'en-us', getter=language_getter_mock)
+        assert Action is MyAction
 
 def test_action_registry_suitable_for_returns_my_action_without_language_item():
+    
+    mocker = Mocker()
+    
     class MyActionNoLanguage(ActionBase):
         regex = r'^I do (\w+)\s(\w+) so proudly$'
         def execute(self, context, *args, **kw):
             pass
 
-    language_getter_mock = Mock()
-    language_getter_mock.expects(at_least_once()).method('get').will(return_value('^$'))
-    Action, args, kwargs = ActionRegistry.suitable_for('I do unit test so proudly', 'en-us', getter=language_getter_mock)
-    assert Action is MyActionNoLanguage
+    language_getter_mock = mocker.mock()
+    language_getter_mock.get(ANY)
+    mocker.count(min=1, max=None)
+    mocker.result('^$')
+    
+    with mocker:
+        Action, args, kwargs = ActionRegistry.suitable_for('I do unit test so proudly', 'en-us', getter=language_getter_mock)
+        assert Action is MyActionNoLanguage
 
 def test_action_registry_can_resolve_same_name_classes():
+    
+    mocker = Mocker()
+    
     class MyActionSameName(ActionBase):
         regex = r'I do (\w+) very well'
         def execute(self, context, *args, **kw):
@@ -109,75 +130,108 @@ def test_action_registry_can_resolve_same_name_classes():
             pass
     Temp2 = MyActionSameName
 
-    language_getter_mock = Mock()
-    language_getter_mock.expects(at_least_once()).method('get').will(return_value('^$'))
+    language_getter_mock = mocker.mock()
+    language_getter_mock.get(ANY)
+    mocker.count(min=1, max=None)
+    mocker.result('^$')
 
-    Action1, args1, kwargs1 = ActionRegistry.suitable_for('I do test very well', 'en-us', getter=language_getter_mock)
-    Action2, args2, kwargs2 = ActionRegistry.suitable_for('I do test very bad', 'en-us', getter=language_getter_mock)
-    assert Action1 is not MyActionSameName
-    assert Action1 is not Temp2
-    assert Action1 is Temp1
-    assert Action2 is Temp2
-    assert Action2 is MyActionSameName
+    with mocker:
+        Action1, args1, kwargs1 = ActionRegistry.suitable_for('I do test very well', 'en-us', getter=language_getter_mock)
+        Action2, args2, kwargs2 = ActionRegistry.suitable_for('I do test very bad', 'en-us', getter=language_getter_mock)
+        assert Action1 is not MyActionSameName
+        assert Action1 is not Temp2
+        assert Action1 is Temp1
+        assert Action2 is Temp2
+        assert Action2 is MyActionSameName
 
 @raises(LanguageDoesNotResolveError)
 def test_action_registry_suitable_for_raises_when_language_getter_can_not_resolve():
+    
+    mocker = Mocker()
+    
     class MyActionLanguage(ActionBase):
         regex = LanguageItem('foo_bar_regex1')
         def execute(self, context, *args, **kw):
             pass
 
-    language_getter_mock = Mock()
-    language_getter_mock.expects(at_least_once()).method('get').will(return_value('^$'))
-    language_getter_mock.expects(once()).get(eq(LanguageItem('foo_bar_regex1'))).will(return_value(None))
-    Action, args, kwargs = ActionRegistry.suitable_for('Something blabla', 'en-us', getter=language_getter_mock)
+    language_getter_mock = mocker.mock()
+    language_getter_mock.get(LanguageItem('foo_bar_regex1'))
+    mocker.count(min=1, max=1)
+    mocker.result(None)
+    language_getter_mock.get(ANY)
+    mocker.count(min=1, max=None)
+    mocker.result('^$')
+    
+    with mocker:
+        Action, args, kwargs = ActionRegistry.suitable_for('Something blabla', 'en-us', getter=language_getter_mock)
 
 @raises(RuntimeError) # A action can not execute itself for infinite recursion reasons :)
 def test_execute_action_will_not_execute_itself():
+    
+    mocker = Mocker()
+    
     class DoSomethingRecursiveAction(ActionBase):
         regex = r'^(And )?I do "(?P<what>\w+)" stuff$'
         def execute(self, context, getter_mock, *args, **kwargs):
             self.execute_action('And I do "recursive" stuff', context, getter=getter_mock)
 
-    language_getter_mock = Mock()
-    language_getter_mock.expects(at_least_once()).method('get').will(return_value('^$'))
+    language_getter_mock = mocker.mock()
+    language_getter_mock.get(ANY)
+    mocker.count(min=1, max=None)
+    mocker.result('^$')
 
-    context_mock = Mock()
-    context_mock.settings = Mock()
-    context_mock.settings.default_culture = "en-us"
+    context_mock = Object(
+        settings=mocker.mock()
+    )
+    context_mock.settings.default_culture
+    mocker.result("en-us")
 
-    dosaction = DoSomethingRecursiveAction()
-    args = []
-    kwargs = dict(what='nothing')
-
-    dosaction.execute(context_mock, getter_mock=language_getter_mock, *args, **kwargs)
-    context_mock.verify()
+    with mocker:
+        dosaction = DoSomethingRecursiveAction()
+        args = []
+        kwargs = dict(what='nothing')
+    
+        dosaction.execute(context_mock, getter_mock=language_getter_mock, *args, **kwargs)
 
 def test_action_base_can_resolve_elements_in_a_given_page():
+    
+    mocker = Mocker()
+    
     class DoOtherThingAction(ActionBase):
         regex="^Do other thing$"
         def execute(self, context, *args, **kwargs):
             self.element = self.resolve_element_key(context, "button", "Something")
 
-    context_mock = Mock()
-    context_mock.current_page = Mock()
-    context_mock.current_page.expects(once()).get_registered_element(eq("Something")).will(return_value("btnSomething"))
+    context_mock = Object(
+        current_page=mocker.mock()
+        )
+    context_mock.current_page.get_registered_element("Something")
+    mocker.count(min=1, max=1)
+    mocker.result("btnSomething")
 
-    action = DoOtherThingAction()
-    action.execute(context_mock)
-    assert action.element == "btnSomething"
+    with mocker:
+        action = DoOtherThingAction()
+        action.execute(context_mock)
+        assert action.element == "btnSomething"
 
 def test_action_base_can_resolve_elements_using_browser_driver():
+    
+    mocker = Mocker()
+    
     class DoOneMoreThingAction(ActionBase):
         regex="^Do other thing$"
         def execute(self, context, *args, **kwargs):
             self.element = self.resolve_element_key(context, "button", "Something")
 
-    context_mock = Mock()
-    context_mock.browser_driver = Mock()
-    context_mock.browser_driver.expects(once()).resolve_element_key(eq(context_mock), eq("button"), eq("Something")).will(return_value("btnSomething"))
-    context_mock.current_page = None
+    context_mock = Object(
+        browser_driver=mocker.mock(),
+        current_page=None
+        )
+    context_mock.browser_driver.resolve_element_key(context_mock, "button", "Something")
+    mocker.count(min=1, max=1)
+    mocker.result("btnSomething")
 
-    action = DoOneMoreThingAction()
-    action.execute(context_mock)
-    assert action.element == "btnSomething"
+    with mocker:
+        action = DoOneMoreThingAction()
+        action.execute(context_mock)
+        assert action.element == "btnSomething"
